3315b9a0905f343f7bbe37589c994c1d7e22d0b6
Freak <39245775+5-4-7-0-3@users.noreply.github.com>
2022-12-22 10:50:22 +0200

    add/cors

--- server/app.js
+++ server/app.js
@@ -9,6 +9,16 @@ const app = express();
 
 setupDb();
 
+app.use(function (req, res, next) {
+    res.set({
+        "Access-Control-Allow-Origin": '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+        'Access-Control-Allow-Headers': 'Version, Authorization, Content-Type',
+        "Content-Type": 'application/json; charset=UTF-8'
+    });
+    next();
+});
+
 app.use(logger('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
